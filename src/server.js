45ccd5be90a192e97ebc96a1e99fb86481d62f78
Fizz <23521155@gm.uit.edu.vn>
2025-10-08 20:13:42 +0700

    Create code base

---
+++ src/server.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import { env } from '~/config/environment';
+
+import exitHook from 'async-exit-hook';
+import { CLOSE_DB, CONNECT_DB, GET_DB } from '~/config/mongodb';
+import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware';
+
+// import { APIs_V1 } from '~/routes/v1';
+import { corsOptions } from '~/config/cors';
+import cors from 'cors';
+
+const START_SERVER = () => {
+    const app = express();
+    //xu ly cors
+    app.use(cors(corsOptions));
+
+    //Enable req.body json data
+    app.use(express.json());
+
+    // // use APIs_V1
+    // app.use('/v1', APIs_V1);
+
+    // middlieware xu ly loi tap trung
+    app.use(errorHandlingMiddleware);
+
+    const HOST = env.APP_HOST || 'localhost';
+    const PORT = env.APP_PORT || 8017;
+
+    app.get('/', (req, res) => {
+        // Test Absolute import mapOrder
+        res.end('<h1>Hello World!</h1><hr>');
+    });
+
+    app.listen(PORT, HOST, () => {
+        // eslint-disable-next-line no-console
+        console.log(`✅ Server is running at http://${HOST}:${PORT}/`);
+    });
+    exitHook((signal) => {
+        CLOSE_DB();
+    });
+};
+
+CONNECT_DB()
+    .then(() => console.log('Connected to MongoDB'))
+    .then(() => START_SERVER())
+    .catch((e) => {
+        console.error(e);
+        process.exit(0);
+    });
---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+import { env } from '~/config/environment';
+//whitelist domain nhung domain dc truy cap
+export const WHITELIST_DOMAINS = [
+    'http://localhost:5173',
+    'https://mern-murex-nu.vercel.app',
+    // them domain];
+];
+
+export const WEBSITE_DOMAIN =
+    env.BUILD_MODE === 'production' ? env.WEBSITE_DOMAIN_PRODUCTION : env.WEBSITE_DOMAIN_DEVELOPMENT;
